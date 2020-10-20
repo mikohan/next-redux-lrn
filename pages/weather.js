b083b1af22f45cbb1bcc21e3538d1e8e34824331
mikohan <angara99@gmail.com>
2020-10-20 17:41:23 +0300

    Finished redux

--- pages/weather.js
+++ pages/weather.js
@@ -1,9 +1,22 @@
 import { connect } from 'react-redux';
 import { setInfo } from '../redux/actions/main';
+import { useState } from 'react';
 
 function Weather(props) {
-	console.log(props);
-	return <div></div>;
+	const { userInfo, setInfo } = props;
+	const [name, setName] = useState('defName');
+	return (
+		<div>
+			<h1>Some stuff related to user {userInfo[0].name}</h1>
+			<input
+				name="name"
+				type="text"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<button onClick={() => setInfo(name)}>submit</button>
+		</div>
+	);
 }
 
 const mapStateToProps = (state) => ({
